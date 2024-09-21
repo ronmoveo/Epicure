f749b2f109c11414b01eeb96431d634622ba8b68
ronmoveo <ronzil@moveo.co.il>
2024-09-22 00:05:48 +0300

    Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.scss';
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <button className="header__menu-button">
+          <img src="/hamburger.svg" alt="Menu" />
+        </button>
+        <Link to="/" className="header__logo">
+          <img src="/logo.svg" alt="Epicure Logo" />
+        </Link>
+        <div className="header__actions">
+          <button className="header__action-button">
+            <img src="/search.svg" alt="Search" />
+          </button>
+          <button className="header__action-button">
+            <img src="/person.svg" alt="User" />
+          </button>
+          <button className="header__action-button">
+            <img src="/cart.svg" alt="Bag" />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
