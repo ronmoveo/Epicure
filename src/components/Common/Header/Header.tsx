69e3cd69b72250d7465b834c5b188aabdce5d188
ronmoveo <ronzil@moveo.co.il>
2024-10-10 14:53:05 +0300

    Header desktop
    Header desktop finished

--- src/components/Common/Header/Header.tsx
+++ src/components/Common/Header/Header.tsx
@@ -4,23 +4,37 @@ import './Header.scss';
 import NavBar from './NavBar/NavBar';
 import CartModal from './CartModal/CartModal';
 import SearchModal from './SearchModal/SearchModal';
+import { RoutePaths } from '../../../utils/enum';
 
-const Header: React.FC = () => {
-  return (
-    <header className="header">
+const Header: React.FC = () => (
+  <header className="header">
+    <div className="header__mobile-menu">
       <NavBar />
-      <Link to="/" className="header__logo">
+    </div>
+    <div className="header__content">
+      <Link to={RoutePaths.HOME} className="header__logo">
         <img src="/logo.svg" alt="Epicure Logo" />
       </Link>
-      <div className="header__actions">
-        <SearchModal />
-        <button className="header__action-button">
-          <img src="/person.svg" alt="User" />
-        </button>
-        <CartModal />
-      </div>
-    </header>
-  );
-};
+      <Link to={RoutePaths.HOME} className="header__title">
+        EPICURE
+      </Link>
+      <nav className="header__nav">
+        <Link to={RoutePaths.RESTAURANTS} className="header__nav-link">
+          Restaurants
+        </Link>
+        <Link to={RoutePaths.RESTAURANTS} className="header__nav-link">
+          Chefs
+        </Link>
+      </nav>
+    </div>
+    <div className="header__actions">
+      <SearchModal />
+      <button className="header__action-button">
+        <img src="/person.svg" alt="User" />
+      </button>
+      <CartModal />
+    </div>
+  </header>
+);
 
 export default Header;
