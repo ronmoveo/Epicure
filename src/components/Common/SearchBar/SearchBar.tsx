24a07cc67ce1df81cef78f54daff1b3e8d625932
ronmoveo <ronzil@moveo.co.il>
2024-10-12 15:28:03 +0300

    search box fix
    search bar and search modal desktop

--- src/components/Common/SearchBar/SearchBar.tsx
+++ src/components/Common/SearchBar/SearchBar.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 import './SearchBar.scss';
 import { SearchBarProps } from '../../../interfaces';
 
-
-const SearchBar: React.FC <SearchBarProps> = ({ placeholder }) => {
+const SearchBar: React.FC<SearchBarProps> = ({ placeholder }) => {
   return (
     <div className="search-bar">
-      <input 
-        type="text" 
-        className="search-bar__input" 
-        placeholder={placeholder} 
-      />
-      <button className="search-bar__button">
-        <img src="/search.svg" alt="Search" />
-      </button>
+      <div className="search-bar__content">
+        <button className="search-bar__button">
+          <img src="/search.svg" alt="Search" />
+        </button>
+        <input 
+          type="text" 
+          className="search-bar__input" 
+          placeholder={placeholder} 
+        />
+      </div>
     </div>
   );
 };
